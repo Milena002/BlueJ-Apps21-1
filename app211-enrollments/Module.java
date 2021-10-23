a194375fc8e4ae3b8869d781b49b72ab620b54bd
Milena002 <22143664@bucks.ac.uk>
2021-10-23 12:10:02 +0100

    added module

---
+++ app211-enrollments/Module.java
@@ -0,0 +1,34 @@
+
+/**
+ *This class models a BNU module with forms part 
+ *of a course.
+ *
+ * @author Milena Michalska
+ * @version 1.0
+ */
+public class Module
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Module
+     */
+    public Module()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
