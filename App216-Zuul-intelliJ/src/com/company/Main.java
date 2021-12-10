8c562a4eaf796108a0ced79d12cd00a530de837a
Milena002 <22143664@bucks.ac.uk>
2021-12-10 15:12:00 +0000

    updated changes

--- App216-Zuul-intelliJ/src/com/company/Main.java
+++ App216-Zuul-intelliJ/src/com/company/Main.java
@@ -1,8 +1,12 @@
 package com.company;
 
-public class Main {
+public class Main
+{
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args)
+    {
+        Game zuul = new Game();
+        zuul.play();
+
     }
 }
